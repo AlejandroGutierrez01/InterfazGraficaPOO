35279987d941460c097f0e1516d0e7d08bc6143e
AlejandroGutierrez01 <alejandro.gutierrez01@epn.edu.ec>
2024-06-20 09:53:43 -0500

    Primer Commnit

--- src/Form1.java
+++ src/Form1.java
@@ -1,5 +1,13 @@
 import javax.swing.*;
 
 public class Form1 {
+    private JButton button1;
     private JPanel panel1;
+    private JComboBox comboBox1;
+    private JSlider slider1;
+
+    private void createUIComponents() {
+        String[] options = {"A", "B", "C", "D", "E", "F", "G", "H"};
+        // TODO: place custom component creation code here
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Obtiene la entrada del usuario de los dialogos de entrada de JOptionPane
+    String primerNumero = JOptionPane.showInputDialog("Ingrese el primer numero entero");
+    String segundoNumero = JOptionPane.showInputDialog("Ingrese el segundo numero entero");
+    //Convierte las entradas String en valores int para usuario en un calculo
+    int numero = Integer.parseInt(primerNumero);
+    int numero2 = Integer.parseInt(segundoNumero);
+    int suma = numero + numero2; //suma números
+    //muestra los resultados en un dialogo de mensajes de JOptionPane
+        JOptionPane.showMessageDialog(null, "La suma es: " + suma, "La suma es", JOptionPane.PLAIN_MESSAGE);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
