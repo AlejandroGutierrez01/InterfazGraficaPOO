274a20fe1430a7a024f55f5f5849f954adc1beeb
Alejandro Gutiérrez <alejandro.gutierrez01@epn.edu.ec>
2024-06-21 11:57:40 -0500

    Avance en la interfaz grafica

--- src/Form1.java
+++ src/Form1.java
@@ -1,13 +1,37 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Form1 {
-    private JButton button1;
-    private JPanel panel1;
-    private JComboBox comboBox1;
-    private JSlider slider1;
+    private JButton btnok;
+    public JPanel Menu;
+    private JLabel Label;
+    private JButton borrarButton;
+    private JTextField saludotxt;
+    private JButton btnCaptTexto;
+
+    public Form1() {
+        btnok.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Label.setText("Alejandro Gutiérrez");
+            }
+        });
+        borrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Label.setText("");
+            }
+        });
+        btnCaptTexto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Label.setText(saludotxt.getText());
+            }
+        });
+    }
 
     private void createUIComponents() {
-        String[] options = {"A", "B", "C", "D", "E", "F", "G", "H"};
         // TODO: place custom component creation code here
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Obtiene la entrada del usuario de los dialogos de entrada de JOptionPane
+    /*    //Obtiene la entrada del usuario de los dialogos de entrada de JOptionPane
     String primerNumero = JOptionPane.showInputDialog("Ingrese el primer numero entero");
     String segundoNumero = JOptionPane.showInputDialog("Ingrese el segundo numero entero");
     //Convierte las entradas String en valores int para usuario en un calculo
@@ -19,6 +19,11 @@ public class Main {
         JOptionPane.showMessageDialog(null, "La multiplicacion es: " + multiplicacion, "MULTIPLICACION", JOptionPane.PLAIN_MESSAGE);
         double division = numero1 / numero2;
         JOptionPane.showMessageDialog(null, "La division es: " + division, "MULTIPLICACION", JOptionPane.PLAIN_MESSAGE);
-
+    */
+        JFrame menu = new JFrame("mi primera aplicacion");
+        menu.setContentPane(new Form1().Menu);
+        menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//mata todos los procesos, se muestra como un boton x
+        menu.pack();
+        menu.setVisible(true);
     }
 }
